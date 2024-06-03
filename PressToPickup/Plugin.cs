038fedde83f2132b24a70abbfda67063388b092a
Moffein <22742925+Moffein@users.noreply.github.com>
2024-06-03 16:30:37 -0700

    Add project files.

---
+++ PressToPickup/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+
+namespace PressToPickup
+{
+    [BepInPlugin("com.Moffein.PressToPickup", "PressToPickup", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.GenericPickupController.OnTriggerStay += (orig, self, other) =>
+            {
+                return;
+            };
+        }
+    }
+}
