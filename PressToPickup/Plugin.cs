cbb3e9da277347db2f6e609a95b1c548eb3d0225
Moffein <22742925+Moffein@users.noreply.github.com>
2025-11-19 20:28:52 -0600

    DLC3 update, no longer affects temp items

--- PressToPickup/Plugin.cs
+++ PressToPickup/Plugin.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 namespace PressToPickup
 {
-    [BepInPlugin("com.Moffein.PressToPickup", "PressToPickup", "1.0.0")]
+    [BepInPlugin("com.Moffein.PressToPickup", "PressToPickup", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public void Awake()
         {
             On.RoR2.GenericPickupController.OnTriggerStay += (orig, self, other) =>
             {
-                return;
+                if (self.pickup != null && self.pickup.isTempItem)
+                {
+                    orig(self, other);
+                }
             };
         }
     }
